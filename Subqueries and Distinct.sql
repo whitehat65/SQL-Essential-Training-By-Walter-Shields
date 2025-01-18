cb254d77585c0e5ceccee5c70f2e1bd7c4972f89
Prashant Shahaji Hatekar <prashanthatekar5585@gmail.com>
2025-01-18 12:34:24 +0530

    Create Subqueries and Distinct.sql

---
+++ Subqueries and Distinct.sql
@@ -0,0 +1,20 @@
+/*
+	Created By : Prashant Hatekar
+	Created Date : 18/01/2025
+	Description : Subqueries and DISTINCT || Which tracks are not selling?
+*/
+
+SELECT
+	TrackId,
+	Name
+FROM
+	Track
+WHERE
+	TrackId NOT IN
+		(Select
+			DISTINCT
+			TrackId
+		FROM
+			InvoiceLine
+		ORDER BY
+			TrackId)
